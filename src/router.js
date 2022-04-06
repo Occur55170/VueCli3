d6a80556656dc2223dae5d92a367cc019a2fe940
Occur55170 <42513736+Occur55170@users.noreply.github.com>
2022-04-06 22:44:55 +0800

    Home ok

--- src/router.js
+++ src/router.js
@@ -8,16 +8,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Customer',
-      component: Customer
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Customer,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: () => import('./views/Front-End/Home.vue')
+        }
+      ]
     }
   ]
 })
