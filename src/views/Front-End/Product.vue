09a8b6665cb7017af7f47477631be2095662f017
Occur55170 <42513736+Occur55170@users.noreply.github.com>
2022-04-07 01:32:18 +0800

    前台  -ProductList 90%

---
+++ src/views/Front-End/Product.vue
@@ -0,0 +1,254 @@
+<template>
+  <div>
+    <div class="productContent">
+      <nav aria-label="breadcrumb" role="navigation">
+        <ol class="breadcrumb bg-transparent pl-0">
+          <li class="breadcrumb-item">
+            <router-link to="/ProductList/all">產品列表</router-link>
+          </li>
+          <li class="breadcrumb-item">
+            <a href="#" @click.prevent="proCate(product.category)">{{ product.category }}</a>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">{{ product.title }}</li>
+        </ol>
+      </nav>
+
+      <div class="d-flex flex-wrap align-items-start">
+        <img :src="product.imageUrl" class="proImg" :alt="product.title">
+        <div class="proCon">
+          <h1>{{ product.title }}</h1>
+          <h4 class="card-text">{{ product.description }}</h4>
+          <div class="priceCon">
+            <p class="SellPrice">售價{{ product.origin_price|corrency }}</p>
+            <p class="webPrice">網路價<span>{{ product.price|corrency }}</span></p>
+          </div>
+          <div class="qtyCon" v-if="product.is_enable!==0">
+            <button class="btn mb-0" @click.prevent="qty--" :disabled="qty<2"><i class="fas fa-minus"></i></button>
+            <input type="number" v-model.number="qty"  class="text-center" @blur="qtyblur()">
+            <button class="btn mb-0" @click.prevent="qty++"><i class="fas fa-plus"></i></button>
+          </div>
+          <div class="d-flex align-items-center justify-content-between">
+            <button class="btn bg-or text-white" @click="addCart(product.id,qty)" :disabled="product.is_enable==0">
+              <span v-if="product.is_enable!==0"><i class="fa fa-cart-plus" aria-hidden="true"></i> 加入購物車</span>
+              <span v-if="product.is_enable==0">缺貨中</span>
+            </button>
+            <div class="text-muted text-right">
+              <i class="h3 fab fa-cc-visa"></i>
+              <i class="h3 fab fa-cc-jcb"></i>
+              <i class="h3 fab fa-cc-paypal"></i>
+            </div>
+          </div>
+        </div>
+        <div class="alert alert-secondary mt-4 w-100 instruction" role="alert">
+          <h2 class="text-center">購物說明</h2>
+          <p class="productDesc" id="productDesc">
+          {{ product.content }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+
+export default {
+  data () {
+    return {
+      pid: '',
+      product: {},
+      qty: 1
+    }
+  },
+  methods: {
+    getProduct () {
+      const vm = this
+      vm.$emit('LoadingModel', true)
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${vm.pid}`
+      vm.$http.get(api).then((response) => {
+        vm.$emit('LoadingModel', false)
+        if (response.data.success) {
+          vm.product = response.data.product
+          vm.textFilt()
+          vm.$emit('LoadingModel', false)
+        }
+      })
+    },
+    proCate (categoryName) {
+      this.$router.push(`/ProductList/${categoryName}`)
+    },
+    goOrder () {
+      const vm = this
+      vm.$router.push(`/Checkout`)
+    },
+    textFilt () {
+      const vm = this
+      let prodContent = vm.product.content
+      let newTextAry = []
+      for (let i = 0; prodContent.length > i; i++) {
+        if (prodContent[i] === '♥' && i !== 0) {
+          newTextAry.push('<br>')
+        }
+        newTextAry.push(prodContent[i])
+      }
+      $('#productDesc').html(newTextAry.join(''))
+    },
+    addCart (pid, qty = 1) {
+      const vm = this
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`
+      vm.$emit('LoadingModel', true)
+      vm.$http.post(api, { 'data': { 'product_id': pid, 'qty': qty } }).then((response) => {
+        vm.$emit('getcart', '已成功將商品加入購物車')
+      })
+    },
+    qtyblur () {
+      const vm = this
+      if (vm.qty < 1 || vm.qty === false) {
+        vm.qty = 1
+      }
+    }
+  },
+  created () {
+    this.pid = this.$route.params.id
+    this.getProduct()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.productContent{
+  width:900px;
+  margin:0 auto;
+  padding:20px 0;
+  nav{
+    margin-bottom:20px;
+  }
+  .proImg{
+    width:45%;
+  }
+  .proCon{
+    margin-left:5%;
+    width:50%;
+    h1{
+      margin-bottom:10px;
+      font-weight:bold;
+    }
+    h4{
+      margin-bottom:10px;
+    }
+    i{
+      margin-bottom:0 !important;
+    }
+    .card-text{
+      font-size:18px;
+      line-height:1.6;
+    }
+    .priceCon{
+      display:flex;
+      justify-content:flex-start;
+      align-items:center;
+      margin-bottom:20px;
+      .SellPrice{
+        opacity:0.8;
+        margin-top:0 !important;
+        margin-bottom:0 !important;
+        text-decoration:line-through;
+        font-size:18px;
+      }
+      .webPrice{
+        margin:0 20px !important;
+        font-size:30px;
+        span{
+          font-weight:bold;
+          color:#dc3545;
+          font-size:40px;
+        }
+      }
+    }
+    .qtyCon{
+      display:flex;
+      align-items:stretch;
+      margin-bottom:60px;
+      & button{
+        margin:0 5px;
+        font-weight:bold;
+        font-size:20px;
+      }
+    }
+  }
+  .instruction{
+    .productDesc{
+      margin:10px 0;
+      line-height:2;
+      font-size:18px;
+    }
+  }
+}
+@media(max-width:768px){
+  .productContent{
+    width:100%;
+    padding:20px;
+    nav{
+      font-size:16px;
+      ol{
+        margin-bottom:0;
+      }
+    }
+    .proImg{
+      width:45%;
+    }
+  }
+}
+@media(max-width:600px){
+  .productContent{
+    width:100%;
+    padding:20px;
+    nav{
+      font-size:16px;
+      margin-bottom:10px;
+      ol{
+        margin-bottom:0;
+      }
+    }
+    .proImg{
+      width:100%;
+      margin-bottom:20px;
+    }
+    .proCon{
+      width:100%;
+      margin-top: 0;
+      margin-left: 0;
+      margin-bottom:50px;
+      h1{
+        font-size:28px;
+        font-weight:bold;
+      }
+      .card-text{
+        font-size: 16px;
+      }
+      .priceCon{
+        justify-content: space-between;
+        margin-bottom:30px;
+        .webPrice{
+          font-size:40px;
+          span{
+            font-size:40px;
+          }
+        }
+      }
+      .qtyCon{
+        margin-bottom:20px;
+        justify-content:center;
+      }
+    }
+    .instruction{
+      .productDesc{
+        margin:10px 0;
+        line-height:1.6;
+        font-size:16px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Front-End/ProductList.vue
@@ -0,0 +1,306 @@
+<template>
+<div>
+
+  <div class="productListOrder pt-3">
+    <!-- banner -->
+    <div class="jumbotron jumbotron-fluid jumbotron-bg d-flex align-items-end productListBN">
+      <div class="container">
+        <h1 class="display-3 font-weight-bold text-white">線上訂購</h1>
+      </div>
+    </div>
+    <!-- banner -->
+
+    <!-- main -->
+    <div class="container mainContant mb-1 d-flex justify-content-between flex-wrap">
+
+        <!-- 類別選單 (List group) -->
+        <div class="list-group pb-2 text-left">
+          <a href="#" class="h5 list-group-item font-weight-bold list-group-item-action list-group-item-light mb-0" @click.prevent="changGroup('all')">全部顯示</a>
+          <a href="#" class="h5 list-group-item font-weight-bold list-group-item-action list-group-item-light mb-0" v-for="(item,key) in GList" :key="key" @click.prevent="changGroup(item)">{{ item }}</a>
+        </div>
+        <!-- 類別選單 (List group) -->
+
+        <!-- 主要商品列表 (Card) -->
+        <div class="productList">
+          <div class="mb-4" v-for="item in proList" :key="item.id">
+            <div class="card border-0 box-shadow text-center h-100">
+              <a href="#" class="card-title" @click.prevent="moreProduct(item.id)">
+                <img class="card-img-top" :src="item.imageUrl" alt="Card image cap">
+              </a>
+              <div class="card-body">
+                <h4>{{ item.title }}</h4>
+                <p class="h6 text-left">{{ item.description }}</p>
+              </div>
+              <div class="card-footer border-top-0 bg-white" v-if="!item.is_enable">
+                <a href="#" class="btn btn-outline-secondary btn-block btn-sm disabled">缺貨中</a>
+              </div>
+              <div class="card-footer border-top-0 bg-white pt-0" v-if="item.is_enable">
+                <div class=" mb-4">
+                  <p class="h5 text-muted text-left mb-1"  v-show="item.origin_price !== item.price"><del>原價 {{ item.origin_price | corrency }}</del></p>
+                  <p class="h4 font-weight-bold">網路價 <span class="h2 text-danger">{{ item.price | corrency }}</span></p>
+                </div>
+                <a href="#" class="btn btn-outline-secondary btn-block btn-sm" @click.prevent="addCart(item.id)">
+                  <i class="fa fa-cart-plus" aria-hidden="true"></i>搶購去
+                </a>
+              </div>
+          </div>
+        </div>
+        <!-- 主要商品列表 (Card) -->
+    </div>
+    <!-- main -->
+  </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      productList: [],
+      modalProduct: {},
+      GroupList: [],
+      sort: ''
+    }
+  },
+  methods: {
+    getProducts (page = 1) {
+      const vm = this
+      vm.$emit('LoadingModel', true)
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products/all`
+      vm.$http.get(api).then((response) => {
+        vm.$emit('LoadingModel', false)
+        if (response.data.success) {
+          vm.productList = response.data.products
+        }
+      })
+    },
+    moreProduct (pid) {
+      const vm = this
+      vm.$router.push(`/product/${pid}`)
+    },
+    changGroup (sortName) {
+      if (this.sort !== sortName) {
+        this.sort = sortName
+        this.$router.push(`/productList/${sortName}`)
+      }
+    },
+    addCart (pid, qty = 1) {
+      const vm = this
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`
+      vm.$emit('LoadingModel', true)
+      vm.$http.post(api, { 'data': { 'product_id': pid, 'qty': qty } }).then((response) => {
+        vm.$emit('getcart', '已成功將商品加入購物車')
+      })
+    }
+  },
+  computed: {
+    GList () {
+      const vm = this
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products/all`
+      vm.$http.get(api).then((response) => {
+        // for( let i = 0 ;i>response.data.products )
+        if (response.data.success) {
+          response.data.products.map(element => {
+            if (vm.GroupList.indexOf(element.category) === -1) {
+              vm.GroupList.push(element.category)
+            }
+          })
+        }
+      })
+      return vm.GroupList
+    },
+    proList () {
+      const vm = this
+      if (vm.sort !== 'all') {
+        return vm.productList.filter(element => {
+          return element.category === vm.sort
+        })
+      } else {
+        return vm.productList
+      }
+    }
+  },
+  created () {
+    this.sort = this.$route.params.sortId
+    this.$emit('closeNavList')
+    this.getProducts()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.productListOrder{
+  background:#f9f9f9;
+  .productListBN{
+    background-image: url(../../assets/images/productListBN.jpg);
+    background-position:center 200%;
+    position:relative;
+    &::after{
+      content:'';
+      position:absolute;
+      top:0;
+      left:0;
+      width:100%;
+      height:100%;
+      background:#000;
+      opacity:0.5;
+    }
+    &>div{
+      position:relative;
+      z-index:5;
+    }
+  }
+  .mainContant{
+    display:flex;
+    justify-content:space-between;
+  }
+  .list-group{
+    width:20%;
+  }
+  .productList{
+    width:78%;
+    display:flex;
+    flex-wrap:wrap;
+    &>div{
+      width:32%;
+      margin:calc((100% - 96%)/6);
+    }
+    & .card-title{
+      position:relative;
+      img{
+        width:100%;
+        height:230px;
+      }
+    }
+    & .card-title:hover::before{
+      content:'查看商品';
+      position:absolute;
+      top:0;
+      left:0;
+      z-index:10;
+      width:100%;
+      height:100%;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      color:#fff;
+      font-size:20px;
+      font-weight:bold;
+    }
+    & .card-title:hover::after{
+      content:'';
+      font-size:20px;
+      font-weight:bold;
+      position:absolute;
+      top:0;
+      left:0;
+      width:100%;
+      height:100%;
+      background:#000;
+      opacity:0.6;
+    }
+    & .card-body{
+      padding:10px;
+      h4{
+        font-weight:bold;
+        color:#f28200;
+        font-size:20px;
+      }
+      .h6{
+        line-height:1.6;
+        margin-bottom:0;
+        font-size:16px;
+      }
+    }
+  }
+}
+@media(max-width:1000px){
+  .productListOrder{
+    .list-group{
+      width:28%;
+    }
+    .productList{
+      width:70%;
+      display:flex;
+      flex-wrap:wrap;
+      &>div{
+        width:48%;
+        margin:calc((100% - 96%)/6);
+      }
+    }
+  }
+}
+@media(max-width:768px){
+  .productListOrder{
+    background:#f9f9f9;
+    .mainContant{
+      display:flex;
+      justify-content:space-between;
+    }
+    .list-group{
+      width:28%;
+    }
+    .productList{
+      width:72%;
+      justify-content:flex-start;
+      &>div{
+        width:48%;
+        margin:calc((100% - 96%)/4);
+      }
+      .card-title img{
+        width: 100%;
+        height: 190px;
+      }
+      & .card-body{
+        padding:10px;
+        .h6{
+          font-size: 15px;
+        }
+        h4{
+          font-size: 20px;
+        }
+      }
+    }
+  }
+}
+@media(max-width:600px){
+  .productListOrder{
+    .mainContant{
+      flex-wrap:wrap;
+    }
+    .list-group{
+      width:90%;
+      flex-flow:row;
+      flex-wrap:wrap;
+      margin:0 auto 20px auto;
+      a{
+        width:48%;
+        margin:2% 1%;
+        border: 1px solid rgba(0,0,0,0.125) !important;
+      }
+    }
+    .productList{
+      width:90%;
+      margin:0 auto;
+      justify-content:center;
+      &>div{
+        width:100%;
+        flex:unset;
+      }
+      .card-title img{
+        width: 100%;
+        height: 260px;
+      }
+      & .card-body{
+        padding:10px;
+        .h6{
+          font-size: 15px;
+        }
+        h4{
+          font-size: 26px;
+        }
+      }
+    }
+  }
+}
+</style>
